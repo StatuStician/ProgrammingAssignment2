d05a9ba39a631fc771c1803d98dea15b551e3bc5
Statustician <hovig.charchaflian@gmail.com>
2014-11-23 14:06:08 -0500

    This is my submission for PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## this function will take a matrix 
+## and assign certain properties of the matrix to four different
+## functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        
+        set<-function(y){
+                x<<-y
+                m<<- NULL
+        }
+        get<-function() x
+        setInverse<-function(inverse) m<<-inverse
+        getInverse<-function() m
+        
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## THis function checks if the inverse of x exists and returns it
+## If it doesn't exist the function calculates it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getInverse()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data, ...)
+        x$setInverse(m)
+        
 }
